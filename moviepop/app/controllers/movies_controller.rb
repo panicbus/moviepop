83e9de4650b0abc94a40e175cac480dae1816558
Seimith <seimith@gmail.com>
2013-11-20 13:36:17 -0800

    Rails

--- moviepop/app/controllers/movies_controller.rb
+++ moviepop/app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   before_filter :authenticate_user, except: [:index, :show]
+
   def index
+    movie_title = params[:movie_title]
+
+    movies = Typhoeus.get(
+      "http://www.omdbapi.com",
+      :params => { :s => params[:movie_title] }
+    )
+    @results = JSON.parse(movies.body)["Search"]
+    # if @results == nil
+    #   redirect_to not_found_path
+    # end
   end
+
 end
