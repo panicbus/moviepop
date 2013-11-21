4870cf3939ee69c408b9231100112988c83906c7
Seimith <seimith@gmail.com>
2013-11-20 18:56:01 -0800

    jquery clear the div and search bar

--- moviepop/app/assets/javascripts/application.js
+++ moviepop/app/assets/javascripts/application.js
@@ -1,21 +1,6 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-
-  // start of the jQuery ready function and makes sure page is loaded and ready
 
   $(function() {
     // alert("test");
@@ -32,23 +17,15 @@
         type: "get",
       }); //end of AJAX request
 
+      // 4. clear the div and the search box
       $('#movies_results').empty();
-
+      $('#search_movies').val("");
       get_request.done(function(data) {
-          // for (var i = 0; i < data.length; i++) {
-          // console.log(data["Search"])
-          // }
-          var item = data["Search"]
-          for (var i = 0; i < item.length ; i++) {
-            console.log(item[i])
-            $("#movies_results").append("<p>"+ item[i]["Title"] + "</p>");
-          }
-
-          // debugger
-          // $("#movies_results").append(item[0]["Title"]);
+        var item = data["Search"]
+        for (var i = 0; i < item.length ; i++) {
+          console.log(item[i])
+          $("#movies_results").append("<p>"+ item[i]["Title"] + " (" + item[i]["imdbID"] + ")</p>");
+        }
       });
-
-      $('#search_movies').empty();
     });
-
   });
