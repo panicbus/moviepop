6f443546f12e9f37a1ecea0c8403060687f9fbfe
Seimith <seimith@gmail.com>
2013-11-20 17:00:58 -0800

    AJAX parsing thru JSON

--- moviepop/app/assets/javascripts/application.js
+++ moviepop/app/assets/javascripts/application.js
@@ -13,3 +13,38 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+  // start of the jQuery ready function and makes sure page is loaded and ready
+
+  $(function() {
+    // alert("test");
+
+    // 1. prevent default action of the form (prevent it from going to the search method in the controller)
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+      // 2. capture the params of the form
+      var query = $('#search_movies').val();
+      // 3. do a "GET" request to sent the params to get info
+      var get_request = $.ajax({
+        url: "http://www.omdbapi.com/?r=JSON&s=" + query,
+        dataType: "json",
+        type: "get",
+      }); //end of AJAX request
+
+      $('#movies_results').empty();
+
+      get_request.done(function(data) {
+          // for (var i = 0; i < data.length; i++) {
+          // console.log(data["Search"])
+          // }
+          var item = data
+          // debugger
+          console.log(item["Search"][0])
+          $("#movies_results").append(item["Search"][0]["Title"]);
+      });
+
+      $('#search_movies').empty();
+    });
+
+  });
