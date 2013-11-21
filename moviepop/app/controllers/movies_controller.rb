2f452757b56641dc4041ef6f601bafe614be8c53
Seimith <seimith@gmail.com>
2013-11-20 16:06:14 -0800

    AJAX request doing API call

--- moviepop/app/controllers/movies_controller.rb
+++ moviepop/app/controllers/movies_controller.rb
@@ -4,38 +4,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @movie = Movie.new
-    @search = params[:smith]
-    respond_to do |format|
-      format.html
-      format.json { render :json => @movies }
-    end
   end
 
-
-  def search
-    @search = params[:smith]
-    @movie = Typhoeus.get(
-      "http://www.omdbapi.com",
-      :params => { :s => params[:smith] }
-      )
-    @mapped_movies = @movie.map do |mov|
-      {
-        :movie => movie
-        # :name => mov[:user][:name],
-        # :screename => mov[:user][:screen_name],
-        # # :timezone => mov[:user][:time_zone],
-        # :text => mov[:text]
-      }
-    end
-    render :json => @mapped_movies
-  end
-
-  def create
-    @movie = Movie.create(params[:movie])
-     respond_to do |format|
-         format.js
-         # This will create the new item via a post request but it will respond with JavaScript
-     end
-  end
 
 end
