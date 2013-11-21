8e35662d4a4d3e3e64628f184d8822e15bd34f3d
Seimith <seimith@gmail.com>
2013-11-21 10:32:51 -0800

    Id number appending

--- moviepop/app/assets/javascripts/application.js
+++ moviepop/app/assets/javascripts/application.js
@@ -23,14 +23,24 @@
 
       get_request.done(function(data) {
         var item = data["Search"]
-        var poster = data["Poster"]
+        // var poster = data["Poster"]
         for (var i = 0; i < item.length ; i++) {
-          console.log(item[i])
-          $("#movies_results").append("<li>" + item[i]["Title"] + " (" + item[i]["imdbID"] + ")</li>");
+          // console.log(item[i])
+          idNumber = item[i]['imdbID']
+          $("#movies_results").append("<li id=" + item[i]['imdbID'] + ">" + item[i]["Title"] + " (" + item[i]["imdbID"] + ")</li>");
           $('li').css('cursor', 'pointer');
-          $('li').on("click", function(){
+          $('li[id]').on("click", function(){
             $('#movies_results').empty();
-            $('#movies_results').append("test")
+            $('#movies_results').append(idNumber)
+
+
+            // var get_request = $.ajax({
+            //   url: "http://www.omdbapi.com/?i=" + "Star",
+            //   dataType: "json",
+            //   type: "get",
+            // }).done(function(){
+            //   alert("test")
+            // });
 
           })
         }
