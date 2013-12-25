4ab5bc9f8b299bc838c2bd78053fa1fcb48895af
Seimith <seimith@gmail.com>
2013-12-25 14:48:28 -0800

    Edits in Routes; Gemfile; Removed unused model.

--- Gemfile
+++ Gemfile
@@ -14,9 +14,9 @@ group :assets do
 end
 
 group :production do
-   gem 'pg'
+  gem 'pg'
 end
 
 group :development do
-   gem 'sqlite3'
+  gem 'sqlite3'
 end
--- app/models/to_watch.rb
+++
@@ -1,5 +0,0 @@
-class ToWatch < ActiveRecord::Base
-  attr_accessible :actors, :director, :genre, :imdbRating, :imdbVotes, :poster, :title, :user_id, :writer, :year
-
-  belongs_to :user
-end
