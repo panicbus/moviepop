97343bbd16f049bef14e0a0d7fc11d9ce11811da
Seimith <seimith@gmail.com>
2014-02-23 15:07:17 -0800

    Refactored css

--- app/assets/javascripts/04viewdeletefavs.js
+++ app/assets/javascripts/04viewdeletefavs.js
@@ -1,10 +1,13 @@
+// 1. AFTER DOM LOADS
 $(document).ready(function() {
 
-    // var showFavorites = function(){
+  // 2. EVENT HANDLER TO LOOK FOR CLICK EVENT TO SEE USER'S FAVORITES
     $('#see_favorites').on('click', function(){
+
+      // 3. CLEAR OUT THE DIV WITH THE EXISTING LIST OF FAVORITE MOVIES
       $('#movies_results').empty();
 
-      // showFavorites();
+      // 4. getJSON/AJAX REQUEST TO GRAB USER'S FAVORITES IN OUR DATABASE, NOT QUERYING THE API
         $.getJSON("/favorite").done(function(favs){
           if (favs.length > 0) {
             $('#see_favorites').hide().css({"background": "#CCC"}).html("View All Your Favorites").fadeIn('slow');
@@ -19,24 +22,27 @@ $(document).ready(function() {
             } // end of loop
           } else {
             $('#see_favorites').hide().css({"background": "orange"}).html("No Movies In Your Favorites!").fadeIn('slow');
-          }
-        }) //end of getJSON func
+          } // end of if/else conditional
+        }) //end of getJSON to grab movie info
 
-        //////DELETE BUTTON//////
+      // 5. DELETE EVENT HANDLER TO LOOK FOR CLICK EVENT TO DELETE A MOVIE
       $('#movies_results').on('click', '#del_button', function(){
         var id = $(this).attr("data-id");
+        // 6. AJAX REQUEST TO DELETE USER'S FAVORITES
         $.ajax({
           url: "/movies/"+id,
           method: "DELETE",
-        }).done(function(){
-          var item_id = "#" + id;
-          $("*[id=" + id + "]").fadeOut(1000, function(){
-            $(this).remove();
-          });
-        })
-      }); //ends delete
+          success: function(){
+            var item_id = "#" + id;
+            $("*[id=" + id + "]").fadeOut(500, function(){
+              $(this).remove();
+            });
+          } // end of success in ajax
+        }) // end of ajax request to delete
+      }); // end of onclick event to delete a movie
 
-    // click movie poster in faves to see details +++++++++++
+    // 7. EVENT HANDLER TO LOOK FOR CLICK EVENT TO SEE MORE INFO
+    // WHEN THE USER CLICKS ON A MOVIE IN THEIR FAVORITES
     $('#movies_results').on('click', 'div[data-method="zed"]', function(){
       var movieIndex = $(this).data('index');
       $.getJSON("/favorite").done(function(favs){
@@ -45,7 +51,6 @@ $(document).ready(function() {
           // console.log("Corresponding Hash:");
           // console.log(favs[movieIndex]);
           $('#movies_results').empty();
-          // $("<div>PUT STUFF HERE!!</div>").hide().appendTo('#movies_results').fadeIn(500)
           $("<div class='poster'>"
               + "<img data-method='img' src=" + favs[movieIndex]["poster"] + ">"
               + "</div>"
@@ -60,7 +65,7 @@ $(document).ready(function() {
               + "<p>" + favs[movieIndex]["plot"] + "</p>"
               + "</div>"
           ).hide().appendTo('#movies_results').fadeIn(1000); // end of append template
-      }) //end of getJSON func
-    })
-  }) // end of see_favorite onclick event handler
+      }) //end of getJSON
+    }) // end of movies_results onclick event
+  }) // end of see_favorite onclick event
 }); // end of document.ready
