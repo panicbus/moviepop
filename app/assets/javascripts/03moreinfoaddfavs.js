fe8964b9930ce4bca12c3253ad6fa4d41ed8a32f
Seimith <seimith@gmail.com>
2014-02-25 01:28:28 -0800

    Fixed looking up more info to use Rotten Tomatoes

--- app/assets/javascripts/03moreinfoaddfavs.js
+++ app/assets/javascripts/03moreinfoaddfavs.js
@@ -7,39 +7,35 @@ $(document).ready(function() {
     // 3. PREVENT DEFAULT OF FULL PAGE REFRESH
     event.preventDefault();
 
-    // 4. AJAX REQUEST TO SEARCH FOR LIST OF MOVIES
+    // 4. AJAX REQUEST TO SEARCH THE MOVIE THAT THE USER CLICKED IN THE LIST OF MOVIES SEARCHED
     // GRAB THE ID FOR THE DATA
+    var movieid = $(this).attr('id');
     $.ajax({
-      url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=nr9twk2r8vfgq5hkm6je8vmh",
-      data: { q: $(this).attr('id') },
+      url: "http://api.rottentomatoes.com/api/public/v1.0/movies/"+movieid+".json?apikey=nr9twk2r8vfgq5hkm6je8vmh",
+      // data: { q: $(this).attr('id') },
+      movieid: $(this).attr('id'),
       content: 'application/jsonp',
       dataType: "jsonp",
       type: "get",
       // 5. AFTER A SUCCESSFUL AJAX REQUEST, APPEND THE INFO
       // ALSO ALLOW USERS TO SAVE THIS INFO TO THEIR 'FAVORITES'
       success: function(dataTwo) {
-        // console.log(dataTwo);
-        // debugger
         // 6. PARSE THRU THE JSON
-        for (var i in dataTwo["movies"]) {
-          console.log(i+": "+dataTwo["movies"][i]['title']);
-          // debugger
-          var poster = dataTwo['movies'][i]['posters']['original'];
-          var title = dataTwo['movies'][i]['title'];
-          var year = dataTwo['movies'][i]['year'];
-          var rating = dataTwo['movies'][i]['mpaa_rating'];
-          var criticScore = dataTwo['movies'][i]['ratings']['critics_score'];
-          var plot = dataTwo['movies'][i]['critics_consensus'];
-          $('#movies_results').append("<div class='poster'>"
-              + "<img data-method='img' src=" + poster + "></div>"
-              + "<div class='plot'><span class='searchedTitle'>" + title + "</span>"
-              + "<p>" + year + "&nbsp;&nbsp;|&nbsp;&nbsp;"
-              + "Rated: " + rating + "</p>"
-              + "<p>IMDB Rating: " + criticScore + "</p>"
-              + "<p>" + plot + "</p>"
-              + "<p><button id='put_favorite'>Add to Your Favorites!</button></p>"
-              + "</div>").hide().fadeIn('fast');
-        }
+        var poster = dataTwo['posters']['original'];
+        var title = dataTwo['title'];
+        var year = dataTwo['year'];
+        var rating = dataTwo['mpaa_rating'];
+        var criticScore = dataTwo['ratings']['critics_score'];
+        var plot = dataTwo['critics_consensus'];
+        $('#movies_results').append("<div class='poster'>"
+            + "<img data-method='img' src=" + poster + "></div>"
+            + "<div class='plot'><span class='searchedTitle'>" + title + "</span>"
+            + "<p>" + year + "&nbsp;&nbsp;|&nbsp;&nbsp;"
+            + "Rated: " + rating + "</p>"
+            + "<p>IMDB Rating: " + criticScore + "</p>"
+            + "<p>" + plot + "</p>"
+            + "<p><button id='put_favorite'>Add to Your Favorites!</button></p>"
+            + "</div>").hide().fadeIn('fast');
         // 7. AJAX REQUEST TO LET USERS SAVE THIS INFO TO THEIR 'FAVORITES'
         // WE INCLUDE THIS BECAUSE WE APPENED A "ADD TO FAVS" BUTTON ABOVE
         $('#put_favorite').on('click', function(){
