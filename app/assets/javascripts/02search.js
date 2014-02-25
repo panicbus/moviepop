29af5c7ba70847ac03a5c5414132b006e2f64c0b
Seimith <seimith@gmail.com>
2014-02-24 20:23:18 -0800

    Incorporated Rotten Tomatoes in the search

--- app/assets/javascripts/02search.js
+++ app/assets/javascripts/02search.js
@@ -10,24 +10,29 @@ $(document).ready(function() {
     // 4. AJAX REQUEST TO SEARCH FOR LIST OF MOVIES
     // GRAB THE VALUE IN THE SEARCH BOX FOR THE DATA
     $.ajax({
-      url: "http://www.omdbapi.com/",
-      data: { s: $('#search_movies').val() },
-      dataType: "json",
+      url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=nr9twk2r8vfgq5hkm6je8vmh",
+      data: { q: $('#search_movies').val() },
+      content: 'application/jsonp',
+      dataType: "jsonp",
       type: "GET",
-      method: "GET",
       // 5. AFTER A SUCCESSFUL AJAX REQUEST, APPEND THE INFO
       success: function(data) {
+        // debugger
         $('#ackbarsearches').hide().fadeIn('slow');
         $('#see_favorites').css({"background": "lightblue"}).html("View All Your Favorites");
         // 6. PARSE THRU THE JSON
-        var item = data["Search"];
+
+        var item = data['movies'];
         for (var i = 0; i < item.length ; i++) {
-          var idNumber = item[i]['imdbID'];
-          var title = item[i]["Title"];
-          var year = item[i]["Year"];
+          var idNumber = "tt"+ item[i]['alternate_ids']['imdb'];
+          var title = item[i]['title'];
+          var year = item[i]["year"];
           $("#movies_results").append("<li id="+idNumber+" class='movie'>"+title+" ("+year+")</li>").hide().fadeIn('fast').css('cursor', 'pointer');
         } // end of for loop
-      } // end of success
+      }, // end of success
+      error: function() {
+        alert("Error");
+      }
     }) // end of ajax request
 
     // 7. CLEAR OUT THE SEARCH BOX
--- app/assets/javascripts/04viewdeletefavs.js
+++ app/assets/javascripts/04viewdeletefavs.js
@@ -9,22 +9,9 @@ $(document).ready(function() {
 
       // TESTING ROTTEN TOMATOES API TO GET IMAGE
       // $.ajax({
-      //   url: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[mykey]&q=Toy+Story+3',
-      //   dataType: "json",
-      //   xhrFields: {
-      //     // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-      //     // This can be used to set the 'withCredentials' property.
-      //     // Set the value to 'true' if you'd like to pass cookies to the server.
-      //     // If this is enabled, your server must respond with the header
-      //     // 'Access-Control-Allow-Credentials: true'.
-      //     withCredentials: 'true'
-      //   },
-      //   headers: {
-      //     // Set any custom headers here.
-      //     // If you set any non-simple headers, your server must include these
-      //     // headers in the 'Access-Control-Allow-Headers' response header.
-      //   },
-      //   type: "get",
+      //   url: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=nr9twk2r8vfgq5hkm6je8vmh&q=Toy+Story+3',
+      //   content: 'application/jsonp',
+      //   dataType: "jsonp",
       //   success: function(data) {
       //     alert("Yay");
       //   },
